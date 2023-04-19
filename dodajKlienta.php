3f576e31396b84e3f40ba630de4e2320729fa3d8
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-04-19 09:19:33 +0200

    Update 19.04.23

--- dodajKlienta.php
+++ dodajKlienta.php
@@ -6,12 +6,10 @@ $numer=$_POST['f_numer'];
 $kod=$_POST['f_kod'];
 $miejscowojsc=$_POST['f_miejscowosc'];
 
-/*include 'dbconfig.php';
+include 'dbconfig.php';
 
     $baza = mysqli_connect($server,$user,$pass,$base) or ('cos nie tak z połączeniem z BD');
     $zapytanie = "INSERT INTO `klienci` (`id`, `nazwa`, `ulica`, `numer`, `kod pocztowy`, `miejscowość`) VALUES (NULL, '$nazwa', '$ulica', '$numer', '$kod', '$miejscowojsc');";
     $result = $baza->query($zapytanie) or die ('bledne zapytanie');
     $baza->close();
-*/
-echo "<li>gfd</li>";
 ?>
