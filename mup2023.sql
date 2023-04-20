f528e8feec41b0249b0ae6ad4780f6eb3b479867
Mateusz Wojtas <mateusz.wojtas.177@gmail.com>
2023-04-20 17:59:53 +0200

    Baza Danych do projektu

---
+++ mup2023.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 20 Kwi 2023, 17:58
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `mup2023`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) NOT NULL,
+  `ulica` varchar(50) NOT NULL,
+  `numer` int(100) NOT NULL,
+  `kod pocztowy` varchar(6) NOT NULL,
+  `miejscowość` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `klienci`
+--
+
+INSERT INTO `klienci` (`id`, `nazwa`, `ulica`, `numer`, `kod pocztowy`, `miejscowość`) VALUES
+(2, 'BBB', 'Oświecenia', 2, '32-660', 'Chełmek'),
+(13, 'Mikrotech', 'Krakowska', 23, '32-600', 'Oświęcim');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `towary`
+--
+
+CREATE TABLE `towary` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) NOT NULL,
+  `opis` varchar(100) NOT NULL,
+  `ilosc` varchar(20) NOT NULL,
+  `cena` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `towary`
+--
+
+INSERT INTO `towary` (`id`, `nazwa`, `opis`, `ilosc`, `cena`) VALUES
+(1, 'Laptop', 'Laptop marki Apple', '3', '6999 zł'),
+(2, 'Telefon', 'Telefon iPhone 14 pro max', '2', '6499 zł');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `towary`
+--
+ALTER TABLE `towary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT dla tabeli `towary`
+--
+ALTER TABLE `towary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
