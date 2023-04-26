fbed4b401e41159fe62a4db05827555a5e88d9af
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-04-26 21:35:25 +0200

    Projekt aktualizacja 26.04

--- home.php
+++ home.php
@@ -1,15 +1,1 @@
 <h2>Serwis sklepu internetowego "Od A do Z"</h2>
-<<<<<<< HEAD
-=======
-<h2 class="center">Zaloguj się</h2>
-
-<form method="POST" action="zaloguj.php" id="zaloguj">
-    <table border="1" class="loguj">
-        <tr><td>Nazwa użytkownika<td></tr>
-        <tr><td><input type="text" name="login"></td></tr>
-        <tr><td>Hasło</td></tr>
-        <tr><td><input type="password" name="haslo"></td></tr>
-        <tr><td><button type="submit">Zaloguj</button></td></tr>
-    </table>
-</form>
->>>>>>> 9a2e64cb9c3fb8c22cf0e6b45ae3e855b721f157
