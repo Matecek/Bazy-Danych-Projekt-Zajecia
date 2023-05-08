4942284c309408d6d0c478913072244d5578ef44
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-05-08 18:12:25 +0200

    Zamiana wszystkiego na echo

--- login.php
+++ login.php
@@ -1,19 +1,20 @@
 <?php
     session_start();
 
-    if((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true)){
-        header('Location: index.php');
-        exit();
-    }
-?>
-<h2 class="center">Zaloguj się</h2>
+   // if((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true)){
+     //   header('Location: index.php');
+      //  exit();
+   // }
 
-<form method="POST" action="zaloguj.php">
-    <table border="1" class="loguj">
-        <tr><td>Nazwa użytkownika<td></tr>
-        <tr><td><input type="text" name="login"></td></tr>
-        <tr><td>Hasło</td></tr>
-        <tr><td><input type="password" name="haslo"></td></tr>
-        <tr><td><input type="submit" value="Zaloguj"></td></tr>
-    </table>
-</form>
+echo "<h2 class='center'>Zaloguj się</h2>";
+
+echo "<form method='POST' action='zaloguj.php'>";
+    echo "<table border='1' class='loguj'>";
+        echo "<tr><td>Nazwa użytkownika<td></tr>";
+        echo "<tr><td><input type='text' name='login'></td></tr>";
+        echo "<tr><td>Hasło</td></tr>";
+        echo "<tr><td><input type='password' name='haslo'></td></tr>";
+        echo "<tr><td><input type='submit' value='Zaloguj'></td></tr>";
+    echo "</table>";
+echo "</form>";
+?>
